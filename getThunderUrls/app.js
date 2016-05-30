3cab480b04f04b927b4592e9a98e9e4334e939e5
aray <aray@doupai.cc>
2016-05-30 20:31:42 +0800

    获取迅雷下载地址时获取对应链接标题

--- getThunderUrls/app.js
+++ getThunderUrls/app.js
@@ -1,38 +1,35 @@
-(function(){
+(function() {
 
-	$(function(){
-		
-		// $('<script src="app.css"></script>').appendTo(document.body);
-		// $('<script src="bootstrap/css/bootstrap.min.css"></script>').appendTo(document.body);
+	$(function() {
 
-		setTimeout(function(){
-			var urls = [];
+		setTimeout(function() {
 
-			$('a[thunderhref]').each(function(){
-				urls.push( $(this).attr('thunderhref') );
+			var thunderhrefs = '';
+
+			$('a[thunderhref]').each(function() {
+				var href = $(this).attr('thunderhref');
+				var title = $(this).text();
+
+				thunderhrefs += "#" + title;
+				thunderhrefs += "\r\n" + "" + href;
+				thunderhrefs += "\r\n\r\n";
+
 			});
 
-			var thunderhrefs = urls.join("\r\n\r\n");
-			localStorage['thunderhrefs'] = thunderhrefs;
-
-			console.log( thunderhrefs );
-
-			if (urls.length > 0){
-				var dialog = $('<div class="weui_dialog_confirm" style="display: none;" id="dialog1"  ><div class="weui_mask"></div><div class="weui_dialog"><div class="weui_dialog_hd"><strong class="weui_dialog_title">已获取到迅雷下载地址</strong></div><div class="weui_dialog_bd"><textarea class="weui_textarea" rows="5" id="_thunderurls_text" >'+thunderhrefs+'</textarea></div><div class="weui_dialog_ft"><a href="javascript:;" class="weui_btn_dialog primary">关闭</a></div></div></div>');
+			if (thunderhrefs !== '') {
+				var dialog = $('<div class="weui_dialog_confirm" style="display: none;" id="dialogHrefs"  ><div class="weui_mask"></div><div class="weui_dialog"><div class="weui_dialog_hd"><strong class="weui_dialog_title">已获取到迅雷下载地址</strong></div><div class="weui_dialog_bd"><textarea class="weui_textarea" rows="5" id="_thunderurls_text" >' + thunderhrefs + '</textarea></div><div class="weui_dialog_ft"><a href="javascript:;" class="weui_btn_dialog primary">关闭</a></div></div></div>');
 				dialog.appendTo(document.body);
 
-				$('#_thunderurls_text').click(function(){
+				$('#_thunderurls_text').click(function() {
 					$(this).get(0).select();
 				});
 
-				$('#dialog1').show().on('click', '.weui_btn_dialog', function () {
-	                $('#dialog1').off('click').hide();
-	            });
-            }
+				$('#dialogHrefs').show().on('click', '.weui_btn_dialog', function() {
+					$('#dialogHrefs').off('click').hide();
+				});
+			}
 
 		}, 1 * 1000);
-
-
 
 	});
 
