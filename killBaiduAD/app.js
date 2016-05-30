fbc5fbb8b05448b3605d418a2154e079d2e17c9e
aray <aray@doupai.cc>
2016-05-30 13:35:39 +0800

    使用 js+css 方式隐藏百度推广链接

--- killBaiduAD/app.js
+++ killBaiduAD/app.js
@@ -1,35 +1,22 @@
-(function(){
+(function() {
 
-	$(function(){
-		
-		for(var o in localStorage){
+	$(function() {
+
+		for (var o in localStorage) {
 			console.log(o);
 		}
 
-		setInterval(function(){
+		setInterval(function() {
 
-			// if (localStorage['right_ad'] === '1'){
-				$('#content_right').html(' 😄 么有广告很舒服吧~ ');
-				$('.rrecom-container,.rrecom-btn-moving').remove();
-			// }
+			$('[data-tuiguang]').each(function() {
+				$(this).parent().parent().remove();
+			});
 
-			// if (localStorage['price_ad'] === '1'){
-
-				$('.leftBlock').remove();
-				
-				$('[data-tuiguang]').each(function(){
-					$(this).parent().parent().remove();
-				});
-
-				$('.aside-inner.bottom-promotion').remove();
-				
-
-				$('div[id]').each(function(){ 
-					if($(this).attr('id').indexOf('30') !== -1) { 
-						$(this).remove() 
-					}
-				});
-			// }
+			$('div[id]').each(function() {
+				if ($(this).attr('id').indexOf('30') !== -1) {
+					$(this).remove();
+				}
+			});
 
 		}, 200);
 
