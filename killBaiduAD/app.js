fb53f7652353c91a0892e6822454f7da2485d2a6
aray <aray@doupai.cc>
2016-05-31 21:01:40 +0800

    修复搜索页面点击链接跳转后, 再次返回搜索页面白屏的 bug

--- killBaiduAD/app.js
+++ killBaiduAD/app.js
@@ -2,20 +2,20 @@
 
 	$(function() {
 
-		for (var o in localStorage) {
-			console.log(o);
-		}
-
 		setInterval(function() {
 
-			$('[data-tuiguang]').each(function() {
-				$(this).parent().parent().remove();
-			});
+			// $('[data-tuiguang]').each(function() {
+			// 	$(this).parent().parent().remove();
+			// });
 
 			$('div[id]').each(function() {
-				if ($(this).attr('id').indexOf('30') !== -1) {
-					$(this).remove();
-				}
+				// if ($(this).attr('id').indexOf('30') !== -1) {
+				// 	$(this).remove();
+				// }
+			});
+
+			$('.rrecom-btn').each(function(){
+				$(this).click();
 			});
 
 		}, 200);
